05a5e784f8297c83f17143c9ab4b4afca2e344e1
Aqdas Ali <aqdasali584@gmail.com>
2025-02-26 18:15:35 +0500

    Conditional Statement

---
+++ ConditionalSt.py
@@ -0,0 +1,21 @@
+print ("Hello World")
+
+# Conditional Statement
+x=10
+y=20
+if x < 30 and y > 10:
+    print(x)
+if x <= 30 or y >= 10:
+    print("Second if")
+
+if x > 30 and y > 10:
+    print(x)
+else:
+    print("Second if else")
+
+if x > 30 and y > 10:
+    print(x)
+elif x >= 10:
+    print("Second elif")
+else:
+    print("Third else")
---
+++ Fnction.py
@@ -0,0 +1,32 @@
+# Functions
+
+def fun():
+    print("This is my first function in python")
+fun()
+
+def fun(x):
+    print(x*2)
+
+fun(2)
+
+def multiply (x):
+    result = x * 4
+    return result
+print(multiply(2))
+
+# Max and Min No's
+print("Max Number is:",max(1,2,3,4,5,6,7,8,9)
+)
+
+print("Min Number is:",min(1,2,3,4,5,6,7,8,9)
+)
+
+# Absolute Numbers
+print("Absolute No is:",abs(-5))
+
+# Lists are immutable means we can't change it:
+list = [1,2,3,4,5]
+print("Lists are :",list)
+print("First index vlaue is:",list[0])
+print("From first to 4th values:",list[0:4])# Is ma akhri wali index shamil ni hoti ha
+
