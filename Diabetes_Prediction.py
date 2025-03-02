6c696cd61de90ea4b30b3a2040455698a7a32dd7
Aqdas Ali <aqdasali584@gmail.com>
2025-03-02 20:25:14 +0500

    numpy in Python

---
+++ Diabetes_Prediction.py
@@ -0,0 +1,14 @@
+#Importing the Dependencies:
+import NumPy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+#Data Collection and Analysis
+#PIMA Diabetes Dataset
+
+#loading the diabetes dataset to a pandas DataFrame
+diabetes_dataset = pd.read_csv('\PythonStarts\diabetes.csv')
+pd.read_csv()
